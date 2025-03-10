826efa08ed1d0ea1812ec475107ad05e70f66c9f
Sankar1217 <kalukurthi2005@gmail.com>
2025-03-10 16:07:47 +0530

    Modified index.css

--- src/Content.js
+++ src/Content.js
@@ -5,9 +5,20 @@ const Content = ()=>{
         const int = Math.floor(Math.random()*3);
         return nmes[int];
     }
+    const displayInstructions = () =>{
+        const instructions = ["Click the button to change the name", "Click the button to change the name", "Click the button to change the name"];
+        const int = Math.floor(Math.random()*3);
+        return instructions[int];
+        
+    }
+    const handleClick = (name) =>{
+        console.log(`${name}Button clicked`);
+    }
     return (
        <main>
         <p>Hello {handleNameChange()}</p>
+        <p>{displayInstructions()}</p>
+        <button onClick={() => handleClick("Sankar")}>Click me</button>
        </main>
     );
 }
